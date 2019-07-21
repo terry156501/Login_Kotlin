c1c8d9d2add778c98bcc1d3c8bb208536df73ec5
terry156501 <terry156501@outlook.com>
2019-07-21 16:00:01 +0800

    Personal Home_01

--- app/src/main/java/com/terry/login_kotlin/SuccessActivity.kt
+++ app/src/main/java/com/terry/login_kotlin/SuccessActivity.kt
@@ -1,13 +1,32 @@
 package com.terry.login_kotlin
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+
+
 
 @SuppressLint("Registered")
 class SuccessActivity: AppCompatActivity() {
+
+    private var mExit:Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_success)
+        init()
     }
+
+    private fun init() {
+        mExit = findViewById<View>(R.id.log_exit_button) as Button
+
+        mExit!!.setOnClickListener {
+            val intent = Intent(this@SuccessActivity,LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
